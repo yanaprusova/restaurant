1a36e180b94015c54890cc3f07b56bbaa86a4836
Yanaprusova <karyaka90@gmail.com>
2019-08-29 07:38:48 +0300

    первый вариант программы (ошибка)

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaAdviser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Добро пожаловать в меню онлайн-ресторана Yana's" );
+            Console.WriteLine( "Вас приветствует онлайн-официант 'Мартино'" );
+            Console.WriteLine( "Как я могу к Вам обращаться?" );
+            String name = Console.ReadLine();
+            Console.WriteLine( "Здравствуйте, " + name + "!" );
+            Console.WriteLine( name + "," + " какой пункт меню Вы выберите: Закуски, Основные блюда, Напитки?" );
+            String menu = Console.ReadLine();
+            if (menu == "закуски" )
+            {
+                Console.WriteLine( "В таком случае рекомендую Вам:");
+                Console.WriteLine( "*Страчетти с рукколой" );
+                Console.WriteLine( "*Салат с мидиями" );
+                Console.WriteLine( "*Карпаччо из осьминога с поке из авокадо и томатов" );
+            }
+            if (menu == "основные блюда " )
+            {
+                Console.WriteLine( "В таком случае рекомендую Вам:" );
+                Console.WriteLine( "*Суп Минестроне" );
+                Console.WriteLine( "*Равиоли" );
+                Console.WriteLine( "*Ризотто" );
+                Console.WriteLine( "*Лазанья" );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
